5486033a7df64724e5a4f6d42d3a597e2d610fba
pawelbogusz81 <pawelbogusz81@gmail.com>
2022-10-19 00:31:04 +0200

    JavaFX. First window.

--- src/main/java/pl/pawelbogusz81/App.java
+++ src/main/java/pl/pawelbogusz81/App.java
@@ -1,14 +1,18 @@
 package pl.pawelbogusz81;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import pl.pawelbogusz81.exceptions.IOCustomException;
 import pl.pawelbogusz81.ui.text.TextUI;
 import pl.pawelbogusz81.util.Properties;
 
 import java.io.IOException;
 
-public class App {
+public class App extends Application {
 
-    private static final TextUI textUI = new TextUI();
+//    private static final TextUI textUI = new TextUI();
 
     public static void main(String[] args) {
 
@@ -17,7 +21,19 @@ public class App {
         } catch (IOException e) {
             throw new IOCustomException(Properties.DATA_DIRECTORY.toString(), "Creating directory", "Creating data directory error");
         }
-        textUI.showSystemInfo();
-        textUI.showMainMenu();
+        Application.launch(args);
+//        textUI.showSystemInfo();
+//        textUI.showMainMenu();
+    }
+
+    public void start(Stage primaryStage){
+        String hotelName = Properties.HOTEL_NAME;
+        int systemVersion = Properties.SYSTEM_VERSION;
+        Label l = new Label("Hello JavaFX.");
+        Scene scene = new Scene(l,800,600);
+        String title = String.format("System rezerwacji hotelu %s (ver. %d)", hotelName, systemVersion);
+        primaryStage.setTitle(title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
