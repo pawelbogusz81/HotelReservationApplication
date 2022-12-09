ebc097e511f6a88042a8638a366ad5038ab0d1f5
pawelbogusz81 <pawelbogusz81@gmail.com>
2022-12-09 23:04:37 +0100

    Added many bed functionality in new room popup.

--- src/main/java/pl/pawelbogusz81/ui/gui/AddNewRoomScene.java
+++ src/main/java/pl/pawelbogusz81/ui/gui/AddNewRoomScene.java
@@ -1,15 +1,20 @@
 package pl.pawelbogusz81.ui.gui;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddNewRoomScene {
 
-    private Scene mainScene;
+    private final Scene mainScene;
+    private final List<ComboBox<String>> comboBoxes = new ArrayList<>();
 
     public AddNewRoomScene(){
 
@@ -17,16 +22,27 @@ public class AddNewRoomScene {
         TextField imputedRoomNumber = new TextField("Wpisz numer pokoju");
         HBox roomNumberRow = new HBox(roomNumberLabel, imputedRoomNumber);
 
-        Label bedTypeLabel = new Label("Rodzaj łóżka");
-        ComboBox bedTypeBox = new ComboBox<>();
-        bedTypeBox.getItems().addAll("Pojedyncze", "Podwójne", "Królewskie");
-        bedTypeBox.setValue("Pojedyncze");
-        HBox bedTypeRow = new HBox(bedTypeLabel, bedTypeBox);
+        Label bedTypeLabel = new Label("Rodzaje łóżek");
+        Button addNewBedButton = new Button("Dodaj");
 
-        VBox box = new VBox(roomNumberRow, bedTypeRow);
+
+        HBox bedTypeRow = new HBox(bedTypeLabel, addNewBedButton);
+
+        VBox bedsVerticalBox = new VBox(bedTypeRow, getComboBox());
+
+        addNewBedButton.setOnAction(event -> bedsVerticalBox.getChildren().add(getComboBox()));
+
+        VBox box = new VBox(roomNumberRow, bedsVerticalBox);
 
         this.mainScene = new Scene(box,640,480);
 
+    }
+
+    private ComboBox<String> getComboBox() {
+        ComboBox<String> bedTypeBox = new ComboBox<>();
+        bedTypeBox.getItems().addAll("Pojedyncze", "Podwójne", "Królewskie");
+        bedTypeBox.setValue("Pojedyncze");
+        return bedTypeBox;
     }
 
     Scene getMainScene() {
